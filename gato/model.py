8747146dc42c1b790c9de4c65ead025bf7b414df
Kye <kye@apacmediasolutions.com>
2023-09-25 11:21:22 -0400

    monumental cleanup no config

--- gato/model.py
+++ gato/model.py
@@ -201,7 +201,7 @@ class FlashAttention(nn.Module):
 
         # pytorch 2.0 flash attn: q, k, v, mask, dropout, causal, softmax_scale
         
-        with torch.backends.cuda.sdp_kernel(**config._asdict()):
+        with torch.backends.cuda.sdp_kernel(**self._asdict()):
             out = F.scaled_dot_product_attention(
                 q, k, v,
                 attn_mask = mask,
@@ -388,11 +388,16 @@ class GatoConfig:
 
 
 class PatchPositionEncoding(nn.Module):
-    def __init__(self, config):
+    def __init__(
+            self, 
+            dim,
+            discretize_depth,
+            img_patch_size
+        ):
         super().__init__()
-        self.embedding_dim = config.layer_width
-        self.discretize_depth = config.discretize_depth
-        self.patch_size = config.img_patch_size
+        self.embedding_dim = self.layer_width
+        self.discretize_depth = self.discretize_depth
+        self.patch_size = self.img_patch_size
 
         self.row_embedding = nn.Embedding(self.discretize_depth, self.embedding_dim)
         self.col_embedding = nn.Embedding(self.discretize_depth, self.embedding_dim)
@@ -420,13 +425,6 @@ class PatchPositionEncoding(nn.Module):
         return input_ids + self.row_embedding(row_pos.long()) + self.col_embedding(col_pos.long())
 
     
-    def get_config(self):
-        config = super(PatchPositionEncoding, self).get_config()
-        config.update({
-            'config': self.config.to_dict(),
-        })
-        return config
-
 
 class ResidualUnit(nn.Module):
     def __init__(self, num_groups: int, filters: int):
@@ -455,21 +453,46 @@ class ResidualUnit(nn.Module):
 
 
 class ResidualEmbedding(nn.Module):
-    def __init__(self, config):
+    def __init__(
+            self, 
+            input_dim,
+            num_group_norm_groups,
+            layer_width,
+        ):
         super().__init__()
+        self.input_dim = input_dim
+        self.num_group_norm_groups = num_group_norm_groups
+        self.layer_width = layer_width
+        self.input_dim = input_dim
 
         self.root_conv = nn.Sequential(
-            nn.Conv2d(in_channels=config.input_dim, out_channels=96, kernel_size=7, stride=2, padding=3),
-            nn.GroupNorm(num_channels=96, num_groups=config.num_group_norm_groups),
+            nn.Conv2d(
+                in_channels=self.input_dim, 
+                out_channels=96, 
+                kernel_size=7, 
+                stride=2, 
+                padding=3
+            ),
+
+            nn.GroupNorm(
+                num_channels=96, 
+                num_groups=self.num_group_norm_groups
+            ),
             nn.GELU()
         )
 
-        self.residual_units = nn.ModuleList([ResidualUnit(num_groups=config.num_group_norm_groups,
+        self.residual_units = nn.ModuleList([ResidualUnit(num_groups=self.num_group_norm_groups,
                                                           filters=96 * 2 ** (i + 1))
                                                           for i in range(3)])
         
-        if config.input_dim != config.layer_width:
-            self.conv_proj = nn.Conv2d(in_channels=96 * 2 ** 3, out_channels=config.layer_width, kernel_size=1, stride=1, padding=0)
+        if self.input_dim != self.layer_width:
+            self.conv_proj = nn.Conv2d(
+                in_channels=96 * 2 ** 3, 
+                out_channels=self.layer_width, 
+                kernel_size=1, 
+                stride=1, 
+                padding=0
+            )
     
 
     def forward(self, images):
@@ -478,77 +501,75 @@ class ResidualEmbedding(nn.Module):
         for unit in self.residual_units:
             x = unit(x)
 
-        if self.config.input_dim != self.config.layer_width:
+        if self.input_dim != self.layer_width:
             x = self.conv_proj(x)
 
         return x
         
 
-    def get_config(self):
-        config = super(ResidualEmbedding, self).get_config()
-        config.update({
-            'config': self.config.to_dict()
-        })
-        return config
-
-
-
 
 class LocalPositionEncoding(nn.Module):
-    def __init__(self, 
-                 config: Union[GatoConfig, Dict[str, Any]], 
-                 trainable=True,
-                 name=None, 
-                 *args, **kwargs):
+    def __init__(
+            self,
+            token_sequence_length,
+            layer_width,
+            config: Union[GatoConfig, Dict[str, Any]], 
+            trainable=True,
+            name=None, 
+            *args, 
+            **kwargs
+        ):
         """
         Appendix C.3. Position Encodings > Local Observation Position Encodings
         """
         super(LocalPositionEncoding, self).__init__()
+        self.token_sequence_length = token_sequence_length
+        self.layer_width = layer_width
 
         if isinstance(config, dict):
             config = GatoConfig(**config)
         self.config = config
-        self.embedding = nn.Embedding(self.config.token_sequence_length, self.config.layer_width)
+        self.embedding = nn.Embedding(self.token_sequence_length, self.layer_width)
 
     def forward(self, inputs):
         obs_pos, obs_mask = inputs
         embed = self.embedding(obs_pos)
 
-        ones = torch.ones((embed.shape[0], 1, self.config.layer_width)).to(embed.device)
+        ones = torch.ones((embed.shape[0], 1, self.layer_width)).to(embed.device)
         obs_mask = obs_mask.float().transpose(-1, -2).matmul(ones)
         return embed * obs_mask
 
-    def get_config(self):
-        config = super(LocalPositionEncoding, self).get_config()
-        config.update({
-            'config': self.config.to_dict()
-        })
-        return config
 
 
 class DiscreteEmbedding(nn.Module):
-    def __init__(self, config):
+    def __init__(
+            self, 
+            embedding_input_size,
+            layer_width,
+            config
+        ):
         super(DiscreteEmbedding, self).__init__()
+        self.embedding_input_size = embedding_input_size
+        self.layer_width = layer_width
         
         if isinstance(config, dict):
             config = GatoConfig(**config)
 
         self.config = config
-        self.embedding = nn.Embedding(self.config.embedding_input_size, self.config.layer_width)
+        self.embedding = nn.Embedding(self.embedding_input_size, self.layer_width)
 
     def forward(self, inputs):
         return self.embedding(inputs)
 
-    def get_config(self):
-        config = super(DiscreteEmbedding, self).get_config()
-        config.update({
-            'config': self.config.to_dict()
-        })
-        return config
     
 
 class PatchEmbedding(nn.Module):
-    def __init__(self, config):
+    def __init__(
+            self, 
+            img_patch_size,
+            input_dim,
+            config
+        ):
         super(PatchEmbedding, self).__init__()
 
         if isinstance(config, dict):
@@ -560,92 +581,109 @@ class PatchEmbedding(nn.Module):
     
     def forward(self, inputs):
         input_ids, (row_pos, col_pos) = inputs
-        patch_size = self.config.img_patch_size
-        depth = self.config.input_dim // (patch_size * patch_size)
+        patch_size = self.img_patch_size
+        depth = self.input_dim // (patch_size * patch_size)
 
         x = input_ids.view(-1, input_ids.size(1), patch_size, patch_size, depth)
         x = self.residual_embedding(x)
         x = self.pos_encoding((x, (row_pos, col_pos)))
         return x
 
-    def get_config(self):
-        return super(PatchEmbedding, self).get_config()
-
-
 
 class ContinousValueTokenizer(nn.Module):
-    def __init__(self, config, mu=100, m=256, bins=1024):
+    def __init__(
+            self, 
+            vocab_size,
+            config, 
+            mu=100, 
+            m=256, 
+            bins=1024
+        ):
         super(ContinousValueTokenizer, self).__init__()
-        if isinstance(config, dict):
-            config = GatoConfig(**config)
+        self.vocab_size = vocab_size
+
         self.config = config
         self.mu = mu
         self.m = m
         self.bins = bins
 
     def forward(self, inputs):
-        return tokenize_continous_value(inputs, self.mu, self.m, self.bins, shift=self.config.vocabulary_size)
+        return tokenize_continous_value(
+            inputs, 
+            self.mu, 
+            self.m, 
+            self.bins, 
+            shift=self.vocab_size
+        )
     
-    def get_config(self):
-        return super(ContinousValueTokenizer, self).get_config()
 
-    
 class TransformerBlock(nn.Module):
-    def __init__(self, config):
+    def __init__(
+            self, 
+            dropout_rate,
+            layer_width,
+            feedforward_hidden_size,
+            config
+        ):
         super(TransformerBlock, self).__init__()
-
-        if isinstance(config, dict):
-            config = GatoConfig(**config)
-        self.config = config
 
         self.attention = FlashAttention(causal=True, dropout=0.1, flash=True)
         
         #may be unnecessary
-        self.dropout = nn.Dropout(config.dropout_rate)
+        self.dropout = nn.Dropout(self.dropout_rate)
 
         self.feed_forward = nn.Sequential(
-            nn.Linear(in_features=config.layer_width, out_features=config.feedforward_hidden_size),
+            nn.Linear(
+                in_features=self.layer_width, 
+                out_features=self.feedforward_hidden_size
+            ),
             nn.GELU(),
-            nn.Dropout(config.dropout_rate),
-            nn.Linear(in_features=config.feedforward_hidden_size, out_features=config.layer_width),
-            nn.Dropout(config.dropout_rate)
+            nn.Dropout(self.dropout_rate),
+            nn.Linear(
+                in_features=self.feedforward_hidden_size, 
+                out_features=self.layer_width
+            ),
+            nn.Dropout(self.dropout_rate)
         )
 
-        self.layer_norm1 = nn.LayerNorm(normalized_shape=config.layer_width, eps=1e-6)
-        self.layer_norm2 = nn.LayerNorm(normalized_shape=config.layer_width, eps=1e-6)
+        self.layer_norm1 = nn.LayerNorm(normalized_shape=self.layer_width, eps=1e-6)
+        self.layer_norm2 = nn.LayerNorm(normalized_shape=self.layer_width, eps=1e-6)
 
 
     def forward(self, inputs):
         x_norm1 = self.layer_norm1(inputs)
         x_attention, _ = self.attention(x_norm1, x_norm1, x_norm1)
         x_dropout = self.dropout(x_attention)
-        x_residual = x_dropout + inputs
 
+        x_residual = x_dropout + inputs
         x_norm2 = self.layer_norm2(x_residual)
         x_ff = self.feed_forward(x_norm2)
+
         x_residual2 = x_ff + x_residual
+
         return x_residual2
 
 
-    def get_config(self):
-        config = super(TransformerBlock, self).get_config()
-        config.update({
-            'config': self.config.to_dict(),
-        })
-        return config
-    
 
 
 
 class Transformer(nn.Module):
-    def __init__(self, config):
+    def __init__(
+            self, 
+            num_transformer_blocks,
+            config
+        ):
         super(Transformer, self).__init__()
 
         if isinstance(config, dict):
             config = GatoConfig(**config)
         self.config = config
 
-        self.encoders = nn.ModuleList([TransformerBlock(config) for _ in range(config.num_transformer_blocks)])
+        self.encoders = nn.ModuleList(
+            [
+                TransformerBlock(config) for _ in range(self.num_transformer_blocks)
+            ]
+        )
 
     def forward(self, inputs):
         x = inputs
@@ -654,29 +692,60 @@ class Transformer(nn.Module):
         
         return x
 
-    def get_config(self):
-        return super(Transformer, self).get_config()
-
 
 class Gato(nn.Module):
-    def __init__(self, config):
+    def __init__(
+            self, 
+            img_patch_size: int = 16,
+            input_dim: int = 768,
+            token_sequence_length: int = 1024,
+            vocabulary_size: int = 32000,
+            actions_size: int = 1024,
+            continuous_values_size: int = 1024,
+            num_transformer_blocks: int = 8,
+            num_attention_heads: int = 24,
+            layer_width: int = 768,
+            feedforward_hidden_size: int = 3072,
+            key_value_size: int = 32,
+            dropout_rate: float = 0.1,
+            num_group_norm_groups: int = 32,
+            discretize_depth: int = 128,
+            local_position_encoding_size: int = 512,
+            max_seq_len: int = 8192,
+        ):
         super(Gato, self).__init__()
+        self.img_patch_size = img_patch_size
+        self.input_dim = input_dim
+        self.token_sequence_length = token_sequence_length
+        self.vocabulary_size = vocabulary_size
+        self.actions_size = actions_size
+        self.continuous_values_size = continuous_values_size
+        self.num_transformer_blocks = num_transformer_blocks
+        self.num_attention_heads = num_attention_heads
+        self.layer_width = layer_width
+        self.feedforward_hidden_size = feedforward_hidden_size
+        self.key_value_size = key_value_size
+        self.dropout_rate = dropout_rate
+        self.num_group_norm_groups = num_group_norm_groups
+        self.discretize_depth = discretize_depth
+        self.local_position_encoding_size = local_position_encoding_size
+        self.max_seq_len = max_seq_len
 
-        if isinstance(config, dict):
-            config = GatoConfig(**config)
-        self.config = config
 
-        self.image_embedding = PatchEmbedding(config)
-        self.discrete_embedding = DiscreteEmbedding(config)
-        self.continuous_encoding = ContinousValueTokenizer(config)
-        self.transformer = Transformer(config)
-        self.local_pos_encoding = LocalPositionEncoding(config)
+        self.image_embedding = PatchEmbedding(self.img_patch_size, self.input_dim)
+        self.discrete_embedding = DiscreteEmbedding(self.embedding_size, self.layer_width)
+        self.continuous_encoding = ContinousValueTokenizer(self.vocabulary_size)
+        self.transformer = Transformer(self.num_transformer_blocks)
+        self.local_pos_encoding = LocalPositionEncoding(
+            self.token_sequence_length,
+            self.layer_width
+        )
 
     def forward(self, inputs):
         input_ids, (encoding, row_pos, col_pos), (obs_pos, obs_mask) = inputs
         encoding = F.one_hot(encoding, num_classes=3).float()
 
-        ones = torch.ones((input_ids.size(0), 1, self.config.layer_width))
+        ones = torch.ones((input_ids.size(0), 1, self.self.layer_width))
         image_embed = self.image_embedding((input_ids, (row_pos, col_pos)))
         image_embed *= encoding[..., 0].unsqueeze(-1).matmul(ones)
 
@@ -692,7 +761,4 @@ class Gato(nn.Module):
 
         hidden_states = self.transformer(embed)
         return hidden_states
-    
-    def get_config(self):
-        return super(Gato, self).get_config()
     
